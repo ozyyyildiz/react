db4464bf528692e80fbefcc4499ed28867f35007
ozyyyildiz <ozyy1996@gmail.com>
2025-04-11 16:55:25 +0300

    redux

---
+++ 01-starting-project-redux-advenced/src/components/Cart/CartButton.js
@@ -0,0 +1,20 @@
+import classes from "./CartButton.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQty = useSelector((state) => state.cart.totalQuantity);
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggleCart());
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQty}</span>
+    </button>
+  );
+};
+
+export default CartButton;
---
+++ 01-starting-project-redux-advenced/src/components/Shop/ProductItem.js
@@ -0,0 +1,41 @@
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+
+const ProductItem = (props) => {
+  const dispatch = useDispatch();
+  const { title, price, description, id } = props;
+
+  const addToCart = () => {
+    
+
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    dispatch(
+      cartActions.addItem({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>${price.toFixed(2)}</div>
+        </header>
+        <p>{description}</p>
+        <div className={classes.actions}>
+          <button onClick={addToCart}>Add to Cart</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ProductItem;
